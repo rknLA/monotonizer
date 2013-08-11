1295ee055999cd5aede960d61fc85f749ab25dfa
R. Kevin Nelson <rkn@rd.io>
2013-08-11 11:46:16 -0400

    cookieSecret in keys.

--- keys.js
+++ keys.js
@@ -3,4 +3,5 @@ module.exports = {
     client_id: 'YOUR SOUNDCLOUD CLIENT ID',
     client_secret: 'YOUR SOUNDCLOUD CLIENT SECRET'
   },
+  cookieSecret: 'obtain cat. instruct cat to walk on keyboard.'
 };
