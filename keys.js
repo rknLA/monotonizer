4c40ab00cda9f24ab707e554ed0b629c60cd4c26
R. Kevin Nelson <rkn@rd.io>
2013-08-11 11:45:04 -0400

    Update keys.

--- keys.js
+++ keys.js
@@ -1,4 +1,6 @@
 module.exports = {
-  googleApiKey: 'YOUR GOOGLE API KEY HERE',
-  mongoUrl: 'mongodb://nodejitsu:e443b7e2389b918633f204e6a9fd6440@linus.mongohq.com:10082/nodejitsudb2207386069'
+  soundcloud: {
+    client_id: 'YOUR SOUNDCLOUD CLIENT ID',
+    client_secret: 'YOUR SOUNDCLOUD CLIENT SECRET'
+  },
 };
